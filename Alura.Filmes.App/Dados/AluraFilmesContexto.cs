a2e3312321aa992b1c7adf5f5b0a58a58b6e666d
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-02 20:06:09 -0300

    Criação da classe contexto e mapeamento da classe Ator utilizananotações explicitas para mapear.

---
+++ Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,15 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AluraFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using System;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+                foreach (var atores in contexto.Atores)
+                {
+                    Console.WriteLine(atores);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
